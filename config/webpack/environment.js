a2d7eacecde77f3097e510ab6199112681891674
Harsh Bhagat <harsh.bhagat@cogoport.com>
2023-06-28 22:45:14 +0530

    minor

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+environment.config.merge({
+  resolve: {
+    alias: {
+      '@dashboard': path.resolve(__dirname, '../../app/javascript/dashboard'),
+    },
+  },
+});
+
 module.exports = environment
